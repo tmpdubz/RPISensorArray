61f51e5edd2f0ec8d2c1c5857519d4fa70769956
Forced-Enthusiasm <t.m.dubiel@gmail.com>
2018-06-24 11:48:32 -0400

    confusion around how pins are labelled

--- sensors/dht11_temp_humid_sensor.py
+++ sensors/dht11_temp_humid_sensor.py
@@ -2,28 +2,24 @@ import Adafruit_DHT
 import argparse
 import json
 
-# Refer to the pin numbers by their pin number since BCM codes change between Pi versions.
-# See https://raspberrypi.stackexchange.com/questions/12966/what-is-the-difference-between-board-and-bcm-for-gpio-pin-numbering
-# GPIO.setmode(GPIO.BOARD)
-
-def get_temperature(pin_to_circuit):
-    print(Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, pin_to_circuit))
 
 def get_humidity(pin_to_circuit):
-    Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, pin_to_circuit)
+    return Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, pin_to_circuit)[0]
+
+
+def get_temperature(pin_to_circuit):
+    return Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, pin_to_circuit)[1]
+
 
 def main():
     parser = argparse.ArgumentParser("get temperature and humidity from DHT11 on pin")
     parser.add_argument('pin',
         help="GPIO.BOARD pin number connecting to the sensor circuit")
     args = parser.parse_args()
-    get_temperature(args.pin)
-    get_humidity(args.pin)
-
-    # data = {'humidity':get_humidity(args.pin), 'temperature':get_temperature(args.pin)}
-    # json_data = json.dumps(data)
-    # print("returning json encoded data:")
-    # print(json_data)
+    data = {'humidity':get_humidity(args.pin), 'temperature':get_temperature(args.pin)}
+    json_data = json.dumps(data)
+    print("returning json encoded data:")
+    print(json_data)
 
 
 main()
