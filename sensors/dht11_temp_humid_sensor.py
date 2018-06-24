a4c775e80404be6405c06402b22e2061a413543e
Forced-Enthusiasm <t.m.dubiel@gmail.com>
2018-06-24 11:39:44 -0400

    testing out some things

--- sensors/dht11_temp_humid_sensor.py
+++ sensors/dht11_temp_humid_sensor.py
@@ -1,4 +1,3 @@
-import sys
 import Adafruit_DHT
 import argparse
 import json
@@ -8,23 +7,23 @@ import json
 # GPIO.setmode(GPIO.BOARD)
 
 def get_temperature(pin_to_circuit):
-    temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, pin_to_circuit)[1]
-    return temperature
-
+    print(Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, pin_to_circuit))
 
 def get_humidity(pin_to_circuit):
-    humidity = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, pin_to_circuit)[0]
-    return humidity
+    Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, pin_to_circuit)
 
 def main():
     parser = argparse.ArgumentParser("get temperature and humidity from DHT11 on pin")
     parser.add_argument('pin',
         help="GPIO.BOARD pin number connecting to the sensor circuit")
     args = parser.parse_args()
-    data = {'humidity':get_humidity(args.pin), 'temperature':get_temperature(args.pin)}
-    json_data = json.dumps(data)
-    print("returning json encoded data:")
-    print(json_data)
-    return json_data
+    get_temperature(args.pin)
+    get_humidity(args.pin)
+
+    # data = {'humidity':get_humidity(args.pin), 'temperature':get_temperature(args.pin)}
+    # json_data = json.dumps(data)
+    # print("returning json encoded data:")
+    # print(json_data)
+
 
 main()
